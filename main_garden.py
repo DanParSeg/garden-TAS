7f5156496dabbcf47037caec27a5b99482c65030
danparseg <daniel.parra.segovia@gmail.com>
2022-04-09 18:41:40 +0200

    funciona

--- main_garden.py
+++ main_garden.py
@@ -3,21 +3,67 @@ import cv2
 import mss
 import numpy as np
 
-def control_mouse():
-    pass
+import mouse
+
+monitor = {"top": 150, "left": 95, "width": 1850, "height": 920}
+scale_percent = 0.25
+mouse_direction=5
+
+def mouse_in_square(squares):
+    global scale_percent, monitor
+    pos=mouse.get_position()
+    
+    for s in squares:
+        margin=7
+        s=list(s)
+        s[0]=s[0]/scale_percent+monitor["left"]-margin
+        s[1]=s[1]/scale_percent+monitor["top"]-margin
+        s[2]=s[2]/scale_percent+monitor["left"]+margin
+        s[3]=s[3]/scale_percent+monitor["top"]+margin
+        s=tuple(s)
+        if(pos[0]>s[0] and pos[1]>s[1] and pos[0]<s[2] and pos[1]<s[3]):
+            return True
+    return False
+
+def control_mouse(squares):
+    global mouse_direction, monitor
+    vertical_move=41
+    print(mouse.get_position())
+    if(mouse_in_square(squares)):
+        print("good")
+        
+        while(mouse_in_square(squares)):
+            mouse.move(mouse_direction, 0,absolute=False, duration=0)
+    if(mouse.get_position()[0]+35>=(monitor["left"]+monitor["width"])):#change line if right
+        print("change line")
+        
+        mouse.move(-20,vertical_move,absolute=False, duration=0)
+        mouse_direction=-5
+    elif(mouse.get_position()[0]-20<monitor["left"]):#change line if left
+        print("change line")
+        
+        mouse.move(20,vertical_move,absolute=False, duration=0)
+        mouse_direction=5
+    else:
+        print("change")
+        
+        pos=mouse.get_position()
+        mouse.move(0, 0, absolute=True, duration=0)
+        mouse.move(pos[0],pos[1], absolute=True, duration=0)
+        time.sleep(0.05)
 
 
 
 with mss.mss() as sct:
     # Part of the screen to capture
-    monitor = {"top": 150, "left": 80, "width": 1850, "height": 920}
+    
 
     template = cv2.imread('4.png', cv2.IMREAD_COLOR)
     template=cv2.cvtColor(np.array(template), cv2.COLOR_RGB2GRAY)
     method = cv2.TM_CCOEFF_NORMED
-    threshold = 0.90
+    threshold = 0.80
 
-    scale_percent = 0.25
+    
     # resize image
     template = cv2.resize(template, (int(template.shape[1] * scale_percent), int(template.shape[0] * scale_percent)))
     h, w = template.shape[:2]
@@ -34,22 +80,24 @@ with mss.mss() as sct:
         image = cv2.resize(image, (int(image.shape[1] * scale_percent), int(image.shape[0] * scale_percent)))
 
         res = cv2.matchTemplate(image, template, method)
+
+        squares=[]
         # fake out max_val for first run through loop
         max_val = 1
         while max_val > threshold:
             min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
 
             if max_val > threshold:
-                #print(max_val)
-                res[max_loc[1]-h//2:max_loc[1]+h//2+1, max_loc[0]-w//2:max_loc[0]+w//2+1] = 0   
+                res[max_loc[1]-h//2:max_loc[1]+h//2+1, max_loc[0]-w//2:max_loc[0]+w//2+1] = 0
                 image = cv2.rectangle(image,(max_loc[0],max_loc[1]), (max_loc[0]+w+1, max_loc[1]+h+1), (0,255,0) )
-            #break
+                squares.append((max_loc[0],max_loc[1], (max_loc[0]+w+1),(max_loc[1]+h+1)))
 
+        print(squares)
         # Display the picture
-        #cv2.imshow('frame', image)
+        cv2.imshow('frame', image)
         print("fps: {}".format(1 / (time.time() - last_time)))
 
-        control_mouse()
+        control_mouse(squares)
 
         # Press "q" to quit
         if cv2.waitKey(1) & 0xFF == ord("q"):
